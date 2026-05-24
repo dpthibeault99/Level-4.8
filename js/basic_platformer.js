70c07656a7f0ebb866bcd2382ab82e3b41828603
dpthibeault99 <dpthibeault@email.neit.edu>
2026-05-24 12:34:06 -0400

    Update basic_platformer.js

--- js/basic_platformer.js
+++ js/basic_platformer.js
@@ -5,177 +5,174 @@ var context;
 var timer;
 var interval;
 var player;
+var hasGoal = false;// added this
 
 
-	canvas = document.getElementById("canvas");
-	context = canvas.getContext("2d");	
 
-	player = new GameObject({x:100, y:canvas.height/2-100});
+canvas = document.getElementById("canvas");
+context = canvas.getContext("2d");	
 
-	platform0 = new GameObject();
-		platform0.width = canvas.width-300;
-		platform0.x = platform0.width/2;
-		platform0.color = "#66ff33";
-		
-	
-	platform1 = new GameObject();
-		platform1.x = 500;
-		platform1.y = platform0.y- platform0.height/2 - platform1.height/2;
-		platform1.color = "#ffff00";
-		
-	platform2 = new GameObject();
-		platform2.width = canvas.width-300;
-		platform2.x = platform0.width/2;
-		platform2.color = "#66ff33";
-		platform2.y = platform0.y- 200;
-		platform2.color = "#66ff33";
+player = new GameObject({x:100, y:canvas.height/2-100});
 
-		
-	
-	goal = new GameObject({width:24, height:50, x:20, y:platform0.y-100, color:"#00ffff"});
+platform0 = new GameObject();
+	platform0.width = canvas.width-300;
+	platform0.x = platform0.width/2;
+	platform0.color = "#66ff33";
 	
 
-	var fX = .85;
-	var fY = .97;
+platform1 = new GameObject();
+	platform1.x = 500;
+	platform1.y = platform0.y- platform0.height/2 - platform1.height/2;
+	platform1.color = "#ffff00";
 	
-	var gravity = 1;
+platform2 = new GameObject();
+	platform2.width = canvas.width-300;
+	platform2.x = platform0.width/2;
+	platform2.color = "#66ff33";
+	platform2.y = platform0.y- 200;
+	platform2.color = "#66ff33";
+
 	
 
-	interval = 1000/60;
-	timer = setInterval(animate, interval);
+goal = new GameObject({width:24, height:50, x:20, y:platform0.y-100, color:"#00ffff"});
+
+
+var fX = .85;
+var fY = .97;
+
+var gravity = 1;
+
+
+interval = 1000/60;
+timer = setInterval(animate, interval);
 
 function animate()
 {
-	
-	context.clearRect(0,0,canvas.width, canvas.height);	
 
-	if(w && player.canJump && player.vy ==0)
-	{
-		player.canJump = false;
-		player.vy += player.jumpHeight;
-	}
+context.clearRect(0,0,canvas.width, canvas.height);	
 
-	if(a)
-	{
-		player.vx += -player.ax * player.force;
-	}
-	if(d)
-	{
-		player.vx += player.ax * player.force;
-	}
+if(w && player.canJump && player.vy ==0)
+{
+	player.canJump = false;
+	player.vy += player.jumpHeight;
+}
 
-	player.vx *= fX;
-	player.vy *= fY;
-	
-	player.vy += gravity;
-	
-	player.x += Math.round(player.vx);
-	player.y += Math.round(player.vy);
-	
-	platform1.x += platform1.vx;
+if(a)
+{
+	player.vx += -player.ax * player.force;
+}
+if(d)
+{
+	player.vx += player.ax * player.force;
+}
 
-	while(platform0.hitTestPoint(player.bottom()) && player.vy >=0)
-	{
-		player.y--;
-		player.vy = 0;
-		player.canJump = true;
-	}
-	while(platform0.hitTestPoint(player.left()) && player.vx <=0)
-	{
-		player.x++;
-		player.vx = 0;
-	}
-	while(platform0.hitTestPoint(player.right()) && player.vx >=0)
-	{
-		player.x--;
-		player.vx = 0;
-	}
-	while(platform0.hitTestPoint(player.top()) && player.vy <=0)
-	{
-		player.y++;
-		player.vy = 0;
-	}
-	while(platform2.hitTestPoint(player.top()) && player.vy <=0)
-	{
-		player.y++;
-		player.vy = 0;
-	}
-	while(platform2.hitTestPoint(player.bottom()) && player.vy >=0)
-	{
-		player.y--;
-		player.vy = 0;
-		player.canJump = true;
-	}
-	while(platform2.hitTestPoint(player.left()) && player.vx <=0)
-	{
-		player.x++;
-		player.vx = 0;
-	}
-	while(platform2.hitTestPoint(player.right()) && player.vx >=0)
-	{
-		player.x--;
-		player.vx = 0;
-	}
-	while(platform1.hitTestPoint(player.top()) && player.vy <=0)
-	{
-		player.y++;
-		player.vy = 0;
-	}
-	while(platform1.hitTestPoint(player.bottom()) && player.vy >=0)
-	{
-		player.y--;
-		player.vy = 0;
-		player.canJump = true;
-	}
-	while(platform1.hitTestPoint(player.left()) && player.vx <=0)
-	{
-		player.x++;
-		player.vx = 0;
-	}
-	while(platform1.hitTestPoint(player.right()) && player.vx >=0)
-	{
-		player.x--;
-		player.vx = 0;
-		if (goal.y >= platform0.y-100)
-		{
-			platform1.y = 9000;
-		}
-	} 
-	// while(platform1.hitTestPoint(player.right()) && player.vx >=0 && goal.y == 10000)
-	// {
-	// 	player.x--;
-	// 	player.vx = 0;
-	// 	platform1.y = 500;
-	// } 
-	
-	//---------Objective: Let Me Out!---------------------------------------------------------------------------------------------------- 
-	//---------Run this program first.
-	//---------Get the pearl to open the door--------------------------------------------------------------------------------------------
-	//---------Hint: you'll need a new variable to keep track of the key-----------------------------------------------------------------
-	
-	if(player.hitTestObject(goal))
-	{
-		goal.y = 10000;
-	}
-	
-	
+player.vx *= fX;
+player.vy *= fY;
 
+player.vy += gravity;
 
+player.x += Math.round(player.vx);
+player.y += Math.round(player.vy);
 
-	
+platform1.x += platform1.vx;
 
-	
-	
-	
-	
-	
-	platform0.drawRect();
-	platform2.drawRect();
-	platform1.drawRect();
-	player.drawRect();
-	
-	//Show hit points
-	player.drawDebug();
-	goal.drawCircle();
+while(platform0.hitTestPoint(player.bottom()) && player.vy >=0)
+{
+	player.y--;
+	player.vy = 0;
+	player.canJump = true;
+}
+while(platform0.hitTestPoint(player.left()) && player.vx <=0)
+{
+	player.x++;
+	player.vx = 0;
+}
+while(platform0.hitTestPoint(player.right()) && player.vx >=0)
+{
+	player.x--;
+	player.vx = 0;
+}
+while(platform0.hitTestPoint(player.top()) && player.vy <=0)
+{
+	player.y++;
+	player.vy = 0;
+}
+while(platform2.hitTestPoint(player.top()) && player.vy <=0)
+{
+	player.y++;
+	player.vy = 0;
+}
+while(platform2.hitTestPoint(player.bottom()) && player.vy >=0)
+{
+	player.y--;
+	player.vy = 0;
+	player.canJump = true;
+}
+while(platform2.hitTestPoint(player.left()) && player.vx <=0)
+{
+	player.x++;
+	player.vx = 0;
+}
+while(platform2.hitTestPoint(player.right()) && player.vx >=0)
+{
+	player.x--;
+	player.vx = 0;
+}
+while(platform1.hitTestPoint(player.top()) && player.vy <=0)
+{
+	player.y++;
+	player.vy = 0;
+}
+while(platform1.hitTestPoint(player.bottom()) && player.vy >=0)
+{
+	player.y--;
+	player.vy = 0;
+	player.canJump = true;
+}
+while(platform1.hitTestPoint(player.left()) && player.vx <=0)
+{
+	player.x++;
+	player.vx = 0;
+}
+// while(platform1.hitTestPoint(player.right()) && player.vx >=0)
+// {
+// 	player.x--;
+// 	player.vx = 0;
+// 	if (goal.y >= platform0.y-100)
+// 	{
+// 		platform1.y = 9000;
+// 	}
+// } 
+
+while(platform1.hitTestPoint(player.right()) && player.vx >0) // Udated
+{
+player.x--;
+player.vx = 0;
+
+if(hasGoal)
+{
+	platform1.y = 9000;
+}
+}
+
+//---------Objective: Let Me Out!---------------------------------------------------------------------------------------------------- 
+//---------Run this program first.
+//---------Get the pearl to open the door--------------------------------------------------------------------------------------------
+//---------Hint: you'll need a new variable to keep track of the key-----------------------------------------------------------------
+
+if(player.hitTestObject(goal))
+{
+hasGoal = true;
+goal.y = 10000;
+}
+
+platform0.drawRect();
+platform2.drawRect();
+platform1.drawRect();
+player.drawRect();
+
+//Show hit points
+player.drawDebug();
+goal.drawCircle();
 }
 
